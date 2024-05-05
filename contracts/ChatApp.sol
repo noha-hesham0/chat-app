dce4a71c412d3bd6210bed1e5e7e0b6ceae3fad1
Noha <noha.mohamed@ejust.edu.eg>
2024-05-05 09:24:45 +0300

    add quotation

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -26,7 +26,7 @@ contract chatApp{
     }
     //create account
     function createAccount(string calldata name) external {
-        require(checkUserExist(msg.sender) == false, "user already exists);
+        require(checkUserExist(msg.sender) == false, "user already exists");
         require(bytes(name).lenght>0, "username cannot be empty");
 
         userList[msg.sender].name= name;
